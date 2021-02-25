c0f6afb7db912965974b1f6295f4d660f9e88add
Anthony Smith <420061+anthonator@users.noreply.github.com>
2021-02-25 08:12:47 -0500

    🥇

---
+++ mix.exs
@@ -0,0 +1,23 @@
+defmodule K8S.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :k8s,
+      version: "0.0.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    []
+  end
+end
